f35c44ce9d1bad9cbbb30b72d93f79dca0a72308
user <user@DESKTOP-ESV96G5>
2023-03-28 13:54:58 +0900

    '230327'

--- Homework/src/homework/Homework230327.java
+++ Homework/src/homework/Homework230327.java
@@ -95,6 +95,7 @@ public class Homework230327 {
 	    /* 추가문제) 아래와 같이 변수 값이 주어질 경우 각 자리의 합을 구해 출력하세요. 
 		    예시) 373 일 경우 3 + 7 + 3 = 13 으로 결과는 13입니다.
 		    힌트) 곱하기 연산자 (*), 나누기 연산자( / ) 와 빼기 연산자 ( - ) 를 이용하세요. */
+		
 		int value = 485;
 		System.out.println(value/100+(value-100*4)/10+value%10);
 		System.out.println(value/100+(value-100*4)/10+(value-10*48));
