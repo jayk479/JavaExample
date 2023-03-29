45d608bca8c9a394cc68b06a64af6312b0d7e02f
user <user@DESKTOP-ESV96G5>
2023-03-29 13:48:12 +0900

    '230328수정'

--- Homework/src/homework/Homework230328.java
+++ Homework/src/homework/Homework230328.java
@@ -144,7 +144,6 @@ public class Homework230328 {
 		
 		boolean run = true;
 		int balance = 0;
-		// int total = 0;
 		while(run) {
 			System.out.println("--------------------------------");
 			System.out.println("1. 예금 | 2. 출금 | 3. 잔고 | 4. 종료");
